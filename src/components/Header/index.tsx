e1a5c26029588f46d050af8ea75b4e2b90bee8ba
thales menzner <thalesmenzz@gmail.com>
2023-02-14 15:04:01 -0300

    finish front-end

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog'
-
 import logo from '../../assets/logo.svg'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
@@ -14,15 +14,7 @@ export function Header() {
                         <NewTransactionButton>Nova transação</NewTransactionButton>
                     </Dialog.Trigger>
 
-                    <Dialog.Portal>
-                    <Dialog.Overlay />
-
-                            <Dialog.Title>
-                                Nova Transação
-                            </Dialog.Title>
-                            <Dialog.Close />
-                        <Dialog.Content/>
-                    </Dialog.Portal>
+                    <NewTransactionModal />
                 </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
